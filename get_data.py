b86f5976c148f9ef299dba178935b2b7ad1fdd1a
David Fumagalli <davidfumo18@gmail.com>
2022-08-30 09:25:12 -0400

    Added .env for api_key

--- get_data.py
+++ get_data.py
@@ -3,3 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import matplotlib.ticker as ticker
+from dotenv import dotenv_values
+
+def get_data():
+    config = dotenv_values(".env")
+    api_key = config['API_KEY']
+    print(api_key)
+
+
+if __name__ == "__main__":
+    get_data()
