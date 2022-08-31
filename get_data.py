fa00c9828b1c7a85d34d72bbf95f7670791909b1
David Fumagalli <davidfumo18@gmail.com>
2022-08-31 10:56:16 -0400

    Implemented and pulled channel data

--- get_data.py
+++ get_data.py
@@ -82,7 +82,7 @@ def get_video_data():
 
     youtube = build('youtube', 'v3', developerKey = apiKey)
     
-    df = pd.DataFrame([], columns=['id', 'title', 'views', 'likes', 'comments'])
+    df = pd.DataFrame([], columns=['id', 'title', 'views', 'likes', 'comments', 'channelId', 'date'])
     for id in channelIds:
         df = pd.concat([df,get_videos_info(youtube, get_channel_videos(youtube, id))], ignore_index=True)
 
@@ -90,7 +90,44 @@ def get_video_data():
     
     df.to_csv("data/video_info.csv")
 
+def get_channel_info(youtube, channelId):
+    response = youtube.channels().list(part='snippet, statistics', id= channelId).execute()
+    return pd.DataFrame([{
+        'id': response['items'][0]['id'],
+        'name': response['items'][0]['snippet']['title'],
+        'totalViews': response['items'][0]['statistics']['viewCount'],
+        'totalSubscribers': response['items'][0]['statistics']['subscriberCount'],
+        'totalVideos': response['items'][0]['statistics']['videoCount'],
+    }])
 
+def clean_channel_info(df):
+    df['totalViews'] = pd.to_numeric(df['totalViews'])
+    df['totalSubscribers'] = pd.to_numeric(df['totalSubscribers'])
+    df['totalVideos'] = pd.to_numeric(df['totalVideos'])
+
+def get_channel_data():
+    config = dotenv_values(".env")
+    apiKey = config['API_KEY']
+    channelIds = [
+        "UC-lHJZR3Gqxm24_Vd_AJ5Yw", # PewDiePie
+        "UCX6OQ3DkcsbYNE6H8uQQuVA", # MrBeast
+        "UCRijo3ddMTht_IHyNSNXpNQ", # Dude Perfect
+        "UC7_YxT-KID8kRbqZo7MyscQ", # Markiplier
+        "UCpB959t8iPrxQWj7G6n0ctQ", # SSSniperWolf
+        "UCTkXRDQl0luXxVQrRQvWS6w", # Dream
+        "UCYzPXprvl5Y-Sf0g4vX-m6g", # jacksepticeye
+        "UCKqH_9mk1waLgBiL2vT5b9g", # VanossGaming
+        "UCwD4x63A9KC7Si2RuSfg-SA", # Dobre Twins
+        "UClQubH2NeMmGLTLgNdLBwXg" # ZHC
+    ]
+
+    youtube = build('youtube', 'v3', developerKey = apiKey)
+    df = pd.DataFrame([], columns=['id', 'name', 'totalViews', 'totalSubscribers', 'totalVideos'])
+    for id in channelIds:
+        df = pd.concat([df,get_channel_info(youtube, id)], ignore_index=True)
+    clean_channel_info(df)
+    
+    df.to_csv("channel_info.csv")
 
 if __name__ == "__main__":
-    pass
+    get_channel_data()
