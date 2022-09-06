e36c7e8acf9409bb25d49d1000e34cf3ae3fff21
David Fumagalli <davidfumo18@gmail.com>
2022-09-06 08:31:21 -0400

    Added more graphs

--- bar_graphs.py
+++ bar_graphs.py
@@ -19,6 +19,8 @@ def most_views_in_set():
 
 def divide_by_views_in_set(totals_row, channels_df, n):
     videos = channels_df.loc[channels_df['id'] == totals_row['channelId'], 'totalVideos'].values[0]
+    if videos > 200:
+        videos = 200
     totals_row['views'] = float(totals_row['views']) / float(videos)
     return totals_row
     
@@ -66,6 +68,53 @@ def most_total_views_per_video():
     plt.barh(labels, views)
     plt.show()
 
+def most_viewed_videos():
+    videos_df = pd.read_csv("data/video_info.csv", index_col= 0)
+    videos_df = videos_df.sort_values('views', ascending= False)[:10]
+    videos_df = videos_df.sort_values('views')
+    
+    labels = [title for title in videos_df['title']]
+    views = [count for count in videos_df['views']]
+
+    fig = plt.figure(figsize= (15,7))
+    plt.rcParams.update({'font.size': 6})
+    plt.barh(labels, views)
+    plt.show()
+
+def most_liked_videos():
+    videos_df = pd.read_csv("data/video_info.csv", index_col= 0)
+    videos_df = videos_df.sort_values('likes', ascending= False)[:10]
+    videos_df = videos_df.sort_values('likes')
+    
+    labels = [title for title in videos_df['title']]
+    likes = [count for count in videos_df['likes']]
+
+    fig = plt.figure(figsize= (15,7))
+    fig.subplots_adjust(left=0.3)
+    plt.barh(labels, likes)
+    plt.show()
+
+def divide_by_views_likes(row):
+    row['likes'] = float(row['likes']) / float(row['views'])
+    return row
+
+def likes_per_view_by_channel():
+    channels_df = pd.read_csv("data/channel_info.csv", index_col= 0)
+    videos_df = pd.read_csv("data/video_info.csv", index_col= 0)
+
+    totals_df = videos_df.groupby(videos_df['channelId'], as_index= False).sum()
+    totals_df = totals_df.apply(divide_by_views_likes, axis= 1)
+    totals_df = totals_df.sort_values('likes')
+
+    ids = [id for id in totals_df['channelId']]
+    labels = [channels_df.loc[channels_df['id'] == id, 'name'].values[0] for id in ids]
+    likes = [totals_df.loc[totals_df['channelId'] == id, 'likes'].values[0] for id in ids]
+    
+    fig = plt.figure(figsize =(12, 7))
+    plt.barh(labels, likes)
+    plt.show()
+
+
 if __name__ == "__main__":
-    most_total_views_per_video()
+    most_liked_videos()
 
